22b884d0913cc8eef00b7df8171813ba9fd03b3a
sngalamou <42877564+sngalamou@users.noreply.github.com>
2019-11-04 14:14:55 -0600

    blub

--- mustang-v1.js
+++ mustang-v1.js
@@ -17,7 +17,7 @@ function initApplication() {
 function loadIndex() {
     // Load the Mustang index file.
     var indexRequest = new XMLHttpRequest();
-    indexRequest.open('GET', 'https://thunderbird-data-sn.azurewebsites.net/kajsfhkshjdk.json');
+    indexRequest.open('GET', 'https://mustang-index.azurewebsites.net/index.json');
     indexRequest.onload = function() {
         console.log("Index JSON:" + indexRequest.responseText);
         document.getElementById("indexID").innerHTML = indexRequest.responseText;
