af359127077b0d51ff2e6cf2c80d1a9e18867399
sngalamou <42877564+sngalamou@users.noreply.github.com>
2019-12-03 10:27:02 -0600

    added gtap

--- gtap.js
+++ gtap.js
@@ -1,8 +1,10 @@
+
 var word;
 var cors = "https://cors-anywhere.herokuapp.com/"
 function initApplication() {
+    var link2 = cors + "https://www.dictionary.com/e/word-of-the-day/"
     var indexRequest = new XMLHttpRequest();
-    indexRequest.open('GET', 'https://www.dictionary.com/e/word-of-the-day/');
+    indexRequest.open('GET', link2);
     indexRequest.onload = function() {
         console.log("website" + indexRequest.responseText);
         document.getElementById("indexID").innerHTML = indexRequest.responseText;
@@ -15,46 +17,25 @@ function initApplication() {
     indexRequest.send();
 }
 
-function loadIndexx(word) {
-    
-    var link = cors + "https://www.linguee.com/english-french/search?source=auto&query=";
-    link += word;
-    link = "\"" + link + "\"";
-    console.log(link);
-
-var indexRequest = new XMLHttpRequest();
-indexRequest.open("GET", "https://www.linguee.com/english-french/search?source=auto&query=bye");
-    console.log("website" + indexRequest.responseText);
-    var trans = (indexRequest.responseText).split(">", 330);
-        console.log(trans[328]);
-        document.getElementById("indexID").innerHTML = trans[328];    
-    document.getElementById("wordID").innerHTML = indexRequest.responseText;
-//xhr.send(data);
-
-}
 function loadIndex(word) {
+    var data = null;
     var link = "https://microsoft-azure-translation-v1.p.rapidapi.com/translate?from=en&to=es&text=";
     link += word;
-    //link = "\"" + link + "\"";
-    console.log(link);
-    var data = null;
 
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+        console.log(this.responseText);
+        document.getElementById("wordID").innerHTML = this.responseText;
 	}
 });
 
-xhr.open("GET", link);
+xhr.open("GET", link );
 xhr.setRequestHeader("x-rapidapi-host", "microsoft-azure-translation-v1.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "0d8ce3de65mshafcf99926226a2cp1f78e5jsn5f88341225c9");
 xhr.setRequestHeader("accept", "application/json");
-console.log(xhr.responseText);
-document.getElementById("wordID").innerHTML = xhr.responseText;
 
 xhr.send(data);
-
 }
